2de5d4eaeaf73cfb347a75881fd34fa934a91778
pedroreis <pedroreiscoder@gmail.com>
2019-07-13 22:55:28 -0300

    Operações CRUD estruturadas em rotas

---
+++ routes/genres.js
@@ -0,0 +1,81 @@
+const Joi = require('joi')
+const express = require('express')
+const router = express.Router()
+
+const genres = [
+    { id: 1, name: 'Action'},
+    { id: 2, name: 'Adventure'},
+    { id: 3, name: 'Comedy'},
+    { id: 4, name: 'Fantasy'},
+    { id: 5, name: 'Horror'},
+    { id: 6, name: 'Romance'},
+    { id: 7, name: 'Science fiction'}
+]
+
+router.get('/', (req, res) => {
+
+    res.send(genres)
+})
+
+router.get('/:id', (req, res) => {
+
+    const genre = genres.find(g => g.id === parseInt(req.params.id))
+
+    if(!genre) return res.status(404).send('There are no genres with this specific ID.')
+
+    res.send(genre)
+})
+
+router.post('/', (req, res) => {
+
+    const { error } = validateGenre(req.body)
+
+    if(error) return res.status(400).send(error.details[0].message)
+ 
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+
+    genres.push(genre)
+
+    res.send(genre)
+})
+
+router.put('/:id', (req, res) => {
+
+    const genre = genres.find(g => g.id === parseInt(req.params.id))
+
+    if(!genre) return res.status(404).send('There are no genres with this specific ID.')
+
+    const { error } = validateGenre(req.body)
+
+    if(error) return res.status(400).send(error.details[0].message)
+
+    genre.name = req.body.name
+
+    res.send(genre)
+})
+
+router.delete('/:id', (req, res) => {
+
+    const genre = genres.find(g => g.id === parseInt(req.params.id))
+
+    if(!genre) return res.status(404).send('There are no genres with this specific ID.')
+
+    const index = genres.indexOf(genre)
+    genres.splice(index, 1)
+
+    res.send(genre)
+})
+
+function validateGenre(genre){
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    return Joi.validate(genre, schema)
+}
+
+module.exports = router
